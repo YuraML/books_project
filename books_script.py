a24aba46b6530ca4ab4eda7ab20272e32c99a7dd
YuraML <malashtayuriy@gmail.com>
2023-02-12 20:31:53 +0400

    create book choice option

--- books_script.py
+++ books_script.py
@@ -1,9 +1,18 @@
 import requests
 import os
+import argparse
 from pathvalidate import sanitize_filename
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, unquote, urlparse
 from pathlib import Path
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description='Скрипт позволяет скачивать книги напрямую с сайта https://tululu.org/')
+    parser.add_argument('start_id', help='укажите id первой скачиваемой книги', type=int, default=20)
+    parser.add_argument('end_id', help='укажите id последней скачиваемой книги', type=int, default=30)
+    return parser
 
 
 def check_for_redirect(response):
@@ -12,10 +21,10 @@ def check_for_redirect(response):
 
 
 def get_book_title(soup, response):
+    check_for_redirect(response)
     title_tag = soup.find('h1')
     title_text = title_tag.text
     book_title = title_text.split('::')
-    check_for_redirect(response)
     return book_title
 
 
@@ -26,16 +35,16 @@ def get_image_link(response, soup, book_site_page_url):
     return book_image_link
 
 
-def get_image_id(response):
+def get_image_id(response, soup, book_site_page_url):
     book_image_link = get_image_link(response, soup, book_site_page_url)
     image_id = unquote(urlparse(book_image_link).path.split('/')[-1])
     image_id = sanitize_filename(image_id)
     return image_id
 
 
-def download_image(response, folder='images/'):
+def download_image(response, soup, book_site_page_url, folder='images/'):
     book_image_link = get_image_link(response, soup, book_site_page_url)
-    image_filename = get_image_id(response)
+    image_filename = get_image_id(response, soup, book_site_page_url)
     Path("images").mkdir(parents=True, exist_ok=True)
     image_path = os.path.join(folder, image_filename)
     response = requests.get(book_image_link)
@@ -44,15 +53,15 @@ def download_image(response, folder='images/'):
         file.write(response.content)
 
 
-def get_book_path(response, folder='books/'):
+def get_book_path(response, soup, folder='books/'):
     filename = sanitize_filename(get_book_title(soup, response)[0])
     book_filename = f'{filename.strip()}.txt'
     book_path = os.path.join(folder, book_filename)
     return book_path
 
 
-def download_txt(response, book_download_url):
-    book_path = get_book_path(response, folder='books/')
+def download_txt(response, soup, book_download_url):
+    book_path = get_book_path(response, soup, folder='books/')
     Path("books").mkdir(parents=True, exist_ok=True)
     response = requests.get(book_download_url)
     response.raise_for_status()
@@ -73,27 +82,33 @@ def get_genres(soup):
     return book_genres
 
 
-def parse_book_page(response):
+def parse_book_page(response, soup, book_site_page_url):
     book_page = {
         'book_name': get_book_title(soup, response)[0].strip(),
         'author': get_book_title(soup, response)[1].strip(),
-        'book_path': get_book_path(response, folder='books/'),
+        'book_path': get_book_path(response, soup, folder='books/'),
         'book_image_link': get_image_link(response, soup, book_site_page_url),
-        'book_image_id': get_image_id(response),
+        'book_image_id': get_image_id(response, soup, book_site_page_url),
         'comments': get_comments(soup),
         'genres': get_genres(soup)
     }
-    print(book_page, sep='\n', end='\n')
+    print(f'Название: {book_page["book_name"]}', f'Автор: {book_page["author"]}', sep='\n', end='\n')
 
 
-if __name__ == '__main__':
-    for book_id in range(1, 11):
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
+    for book_id in range(args.start_id, args.end_id + 1):
         book_download_url = f'https://tululu.org/txt.php?id={book_id}'
         book_site_page_url = f'https://tululu.org/b{book_id}/'
         response = requests.get(book_site_page_url)
         response.raise_for_status()
         soup = BeautifulSoup(response.text, 'lxml')
         try:
-            download_image(response, folder='images/')
+            parse_book_page(response, soup, book_site_page_url)
         except requests.exceptions.HTTPError:
             pass
+
+
+if __name__ == '__main__':
+    main()
