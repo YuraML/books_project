1d27375695d857c65a6d5e70e049a8cf3e6f12c5
YuraML <malashtayuriy@gmail.com>
2023-03-02 17:42:47 +0400

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,28 +1,35 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
+from livereload import Server
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-template = env.get_template('template.html')
 
-with open("json/books_json", "r", encoding="utf8") as my_file:
-    books_json = my_file.read()
-books = json.loads(books_json)
-books_page = []
-for book in books:
-    image = book['book_image_link']
-    author = book['author']
-    title = book['book_name']
-    books_info = {'image': image, 'author': author, 'title': title}
-    books_page.append(books_info)
+def on_reload():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
 
-rendered_page = template.render(books=books_page)
+    with open("json/books_json", "r", encoding="utf8") as my_file:
+        books_json = my_file.read()
+    books = json.loads(books_json)
+    books_page = []
+    for book in books:
+        image = book['book_image_link']
+        author = book['author']
+        title = book['book_name']
+        books_info = {'image': image, 'author': author, 'title': title}
+        books_page.append(books_info)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    rendered_page = template.render(books=books_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+
+on_reload()
+server = Server()
+server.watch('template.html', on_reload)
+
+server.serve(root='.')
